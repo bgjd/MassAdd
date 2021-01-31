589490592752c1e47537628577451713fb4304c9
Ben Davis <ben@bgjd.io>
2021-01-31 16:45:57 +0000

    Add working first version

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # MassAdd
+
+An Anki add on for mass-adding cards from a block of text. Primarily intended for use as part
+of 'sentence mining' when learning a foreign language.
+
+### Installation
+ - Simply copy this folder to your addons21 directory or install using ankiweb
+
+### Usage
+ - Open the MassAdd window from the tools menu
+ - Choose the card type and deck you would like the new cards to be added as. Please
+   note that due to anki requiring the first field of a card be non-blank the first
+   field of the selected card type will be used to add the text.
+ - Copy and paste the text into the window.
+ - Each new line of text will be a seperate card. To make this easier you can
+   use the 'Add' button to seperate the text into new lines based on a specific
+   character, for example you may want to use a full-stop (.) or a comma(,).
+ - Click 'submit' and the cards will be created.
---
+++ __init__.py
@@ -0,0 +1,96 @@
+from aqt import mw, deckchooser, modelchooser
+from anki.notes import Note
+from aqt.utils import showInfo
+from aqt.qt import *
+
+
+class MassAddWindow(QDialog):
+    def __init__(self) -> None:
+        QDialog.__init__(self, None, Qt.Window)
+        self.deck_widget = None
+        self.model_widget = None
+        self.text_edit = None
+        self.processor_widget = None
+        self.processor_layout = None
+        self.processor_label = None
+        self.processor_text = None
+        self.processor_button = None
+        self.submit_button = None
+        self.deck_chooser = None
+        self.model_chooser = None
+
+    def setup_ui(self):
+        layout = QVBoxLayout()
+        self.deck_widget = QWidget(mw)
+        self.model_widget = QWidget(mw)
+        self.text_edit = QTextEdit(mw)
+        self.submit_button = QPushButton(mw)
+        self.deck_chooser = deckchooser.DeckChooser(mw, self.deck_widget)
+        self.model_chooser = modelchooser.ModelChooser(mw, self.model_widget)
+
+        self.processor_widget = QWidget(mw)
+        self.processor_layout = QHBoxLayout(mw)
+        self.processor_label = QLabel("Character to split on:")
+        self.processor_text = QLineEdit(mw)
+        self.processor_text.setMaxLength(1)
+        self.processor_text.setFixedWidth(60)
+        self.processor_button = QPushButton(mw)
+        self.processor_button.setText("Split")
+        self.processor_button.clicked.connect(self.split_text)
+        self.processor_layout.addWidget(self.processor_label)
+        self.processor_layout.addWidget(self.processor_text)
+        self.processor_layout.addWidget(self.processor_button)
+        self.processor_widget.setLayout(self.processor_layout)
+
+        self.submit_button.setText("Add")
+        self.submit_button.clicked.connect(self.add_current_sentences)
+
+        layout.addWidget(self.model_widget)
+        layout.addWidget(self.deck_widget)
+        layout.addWidget(self.processor_widget)
+        layout.addWidget(self.text_edit)
+        layout.addWidget(self.submit_button)
+
+        self.setLayout(layout)
+        self.setWindowTitle("MassAdd")
+        self.setMinimumHeight(300)
+        self.setMinimumWidth(400)
+
+    def show_window(self):
+        if self.submit_button is None:
+            self.setup_ui()
+        self.text_edit.setText("")
+        self.show()
+
+    def split_text(self):
+        text = self.text_edit.toPlainText()
+        split_marker = self.processor_text.text()
+
+        new_text = (split_marker + "\n").join(text.split(split_marker))
+        self.text_edit.setText(new_text)
+        self.processor_text.clear()
+
+    def add_current_sentences(self):
+        deck_id = self.deck_chooser.selectedId()
+        model_id = self.model_chooser.deck.decks.current()["mid"]
+        m = mw.col.models.get(model_id)
+
+        # Use the first field of note as this is required to be non-blank by anki
+        field = m["flds"][0]["name"]
+
+        sentences = self.text_edit.toPlainText().split("\n")
+
+        for s in sentences:
+            note = Note(mw.col, m)
+            note[field] = s.strip()
+            note.model()["did"] = deck_id
+            mw.col.addNote(note)
+
+        showInfo('Done')
+
+
+MAWindow = MassAddWindow()
+
+action = QAction("MassAdd", mw)
+action.triggered.connect(MAWindow.show_window)
+mw.form.menuTools.addAction(action)
